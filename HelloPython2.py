fed54a11f6bc10c72c83aa10d9a61b4a6ebbfc38
uemichi190 <uemichi190@gmail.com>
2025-03-25 11:28:29 +0900

    first commit

---
+++ HelloPython2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# save this as helloHelloPython2.py
+# 行中の#以降は行末までコメントとなる(Python プログラムの実行に影響しない)
+
+def Hello():
+    print("Hello, World!")
+    return
+
+if __name__ == "__main__":
+    Hello()
+    
---
+++ HelloPython3.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# save this as helloHelloPython2.py
+# 行中の#以降は行末までコメントとなる(Python プログラムの実行に影響しない)
+
+def propose(name):
+    print("I love you,{}. Will you marry me?".format(name))
+
+def Hello():
+    for i in range(10):
+        #propose("hoge")
+        propose(i)
+    print("I love you too.")
+
+if __name__ == "__main__":
+    Hello()
+    
